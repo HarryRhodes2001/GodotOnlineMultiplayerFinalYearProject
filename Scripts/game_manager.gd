0e79d6e40f14061bfd32cbfbd015567147c71dd9
HarryRhodes2001 <30061224@students.southwales.ac.uk>
2025-01-09 14:44:38 +0000

    UI Menus Updated

--- Scripts/game_manager.gd
+++ Scripts/game_manager.gd
@@ -1,32 +1,18 @@
 extends Node
 
 @onready var GUI = $GUI
-@onready var GUI2 = $Window/GUI
 @onready var Player = $CharacterBody3D
-@onready var Player2 = $Window/CharacterBody3D2
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	Player2.authority = 2
-	print(Player.authority)
-	print(Player2.authority)
 	
-	if multiplayer.get_unique_id() == 1:
-		select_window(0)  # Player 1 controls window 0
-		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	elif multiplayer.get_unique_id() == 2:
-		select_window(1)  # Player 2 controls window 1
-		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	
 	
 	Player.health_changed.connect(update_health)
 	Player.ammo_changed.connect(update_ammo)
 	Player.killed_player.connect(update_kills)
 	Player.died.connect(update_deaths)
-	Player2.health_changed.connect(update_health2)
-	Player2.ammo_changed.connect(update_ammo2)
-	Player2.killed_player.connect(update_kills)
-	Player2.died.connect(update_deaths)
 
 func select_window(window_index: int):
 	# Set mouse mode based on the selected window
@@ -42,19 +28,11 @@ func _process(delta: float) -> void:
 func update_health(health):
 	GUI.healthUpdate(health)
 
-func update_health2(health):
-	GUI2.healthUpdate(health)
-
 func update_ammo(ammo):
 	GUI.ammoUpdate(ammo)
 
-func update_ammo2(ammo):
-	GUI2.ammoUpdate(ammo)
-
 func update_kills(authority, kills):
 	GUI.killsUpdate(authority, kills)
-	GUI2.killsUpdate(authority, kills)
 
 func update_deaths(authority, deaths):
 	GUI.deathsUpdate(authority, deaths)
-	GUI2.deathsUpdate(authority, deaths)
--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -1,8 +1,16 @@
 extends Control
 
-func _on_play_pressed() -> void:
+func _on_quit_pressed() -> void:
+	get_tree().quit()
+
+
+func _on_client_server_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/world.tscn")
 
 
-func _on_quit_pressed() -> void:
-	get_tree().quit()
+func _on_peerto_peer_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_join_game_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/server_menu.tscn")
