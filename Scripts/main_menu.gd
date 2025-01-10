d196303222a56edbd8a597a93bab4fabe8bee8d3
HarryRhodes2001 <30061224@students.southwales.ac.uk>
2025-01-10 14:06:33 +0000

    Server Creation
    Hopefully this fixes the wireframe perspective. If not, the perspective will have to be set to default for every scene that is present in this commit.

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -1,10 +1,17 @@
 extends Control
 
+const PORT = 57152
+var peer = ENetMultiplayerPeer.new()
+var playerNo = 8
+
 func _on_quit_pressed() -> void:
 	get_tree().quit()
 
-
 func _on_client_server_pressed() -> void:
+	get_tree().get_multiplayer()
+	peer.create_server(PORT, playerNo)
+	multiplayer.multiplayer_peer = peer
+	print(peer)
 	get_tree().change_scene_to_file("res://Scenes/world.tscn")
 
 
